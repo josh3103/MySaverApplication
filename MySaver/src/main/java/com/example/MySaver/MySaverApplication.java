0da7df70e5ee72d3da2c0d1a7dd3bd3368eb6dc5
Joshua61165 <Joshua61165@GB-LD1LPW10G030.mastek.com>
2020-03-18 17:21:01 +0000

    First commit

---
+++ MySaver/src/main/java/com/example/MySaver/MySaverApplication.java
@@ -0,0 +1,13 @@
+package com.example.MySaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySaverApplication.class, args);
+	}
+
+}
---
+++ MySaver/src/test/java/com/example/MySaver/MySaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MySaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
